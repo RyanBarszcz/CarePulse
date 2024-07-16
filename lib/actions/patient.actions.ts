aa487372401dffb70fd5724867b9de4e747dd6a6
RyanBarszcz <113656726+RyanBarszcz@users.noreply.github.com>
2024-07-16 00:24:43 -0400

    Admin passkeyModal

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -83,3 +83,18 @@ export const registerPatient = async ({ identificationDocument, ...patient}: Reg
         console.log(error);
     }
 }
+
+export const getPatient = async (userId: string) => {
+    try {
+        console.log(userId)
+        const patients = await databases.listDocuments(
+            DATABASE_ID!,
+            PATIENT_COLLECTION_ID!,
+            [Query.equal('userId', userId)]
+        );
+
+        return parseStringify(patients.documents[0]);
+    } catch (error) {
+        console.log(error)
+    }
+}
